1e5f0380ba9a71482d791e50f4a19cd72e397c8a
Ansh Deshraj <126981356+anshhdeshraj@users.noreply.github.com>
2023-05-17 01:18:58 +0530

    Update notification.js

--- config/notification.js
+++ config/notification.js
@@ -1,5 +1,5 @@
-const accountSid = 'AC3678aa4a7869060079e3b2e3028a3518';
-const authToken = '69da4b24a239a3dfdac388ed7583103d';
+const accountSid = 'YOUR_SID';
+const authToken = 'YOUR_TOKEN';
 
 const sendSms = (phone,message) => {
   const client = require('twilio')(accountSid, authToken);
